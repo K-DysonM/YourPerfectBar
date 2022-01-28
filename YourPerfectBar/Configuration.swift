0d6ec481ddb6e22704eb45785664ad2163733afd
Kiana Dyson <kdyson@calpoly.edu>
2022-01-28 13:29:01 -0500

    Set Up TableView, SwiftLint, and YelpAPI
    Set up TableViewController using data from the YelpAPI. The YelpAPI grabs data
    on the 20 nearest bars and the tableview uses this data to populate its view.
    Currently showing bar name, bar phone number, and bar image using SDWebImage.
    Finally set up SwiftLint to keep consistency of code styling.

---
+++ YourPerfectBar/Configuration.swift
@@ -0,0 +1,14 @@
+//
+//  Configuration.swift
+//  YourPerfectBar
+//
+//  Created by Kiana Dyson on 1/28/22.
+//
+
+import Foundation
+
+struct Configuration {
+	var yelpApiKey: String {
+		return "s2lp1FNKUx55A8-iPzDgMew8Si4GaRiJSm2UbWw-pDq0ZTpYTNrXZI6C8OE9v9E5_HbiEhV7Hf8X4fDxZRSIseYer7_x2dNBr_-qZavD6JgGn8_ibdTA1d21YW7XYXYx"
+	}
+}
--- YourPerfectBar/ViewController.swift
+++ YourPerfectBar/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import CDYelpFusionKit
+import SDWebImage
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+	let yelpAPIClient = CDYelpAPIClient(apiKey: Configuration().yelpApiKey)
+	var bars = [CDYelpBusiness]()
+	var crasjdlslahsHJJSDH = ""
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		#warning("api calls should be moved off main thread")
+		yelpAPIClient.searchBusinesses(
+			byTerm: "bars",
+			location: "New York City",
+			latitude: nil,
+			longitude: nil,
+			radius: 5000,
+			categories: nil,
+			locale: .english_unitedStates,
+			limit: 20,
+			offset: nil,
+			sortBy: .bestMatch,
+			priceTiers: nil,
+			openNow: nil,
+			openAt: nil,
+			attributes: nil) { [weak self] cdYelpSearchResponse in
+			guard let businesses = cdYelpSearchResponse?.businesses else { return }
+			self?.bars = businesses
+			self?.tableView.reloadData()
+		}
 	}
 
+	// TABLEVIEW METHODS
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		bars.count
+	}
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		#warning("Change cell to a custom cell")
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Bar", for: indexPath)
+		let bar = bars[indexPath.row]
+		cell.textLabel?.text = bar.name
+		cell.detailTextLabel?.text = bar.displayPhone
+		// If not a valid url the placeholder image will be used - Without a placeholder image the imageView will show blank
+		cell.imageView?.contentMode = .scaleAspectFill
+		cell.imageView?.sd_setImage(with: bar.imageUrl, placeholderImage: UIImage(systemName: "music.house"))
+		return cell
+	}
+
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		#warning("Need to set up a view controller show bar detail information")
+		if let name = bars[indexPath.row].name {
+			print("\(name) was clicked")
+		} else {
+			print("\(indexPath.row) was clicked")
+		}
+	}
 
 }
-
