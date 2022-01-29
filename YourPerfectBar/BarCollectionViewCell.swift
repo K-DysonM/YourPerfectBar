b9d3b3a4b697b0bdae86d8c7d32b3b0b8ac290bd
Kiana Dyson <kdyson@calpoly.edu>
2022-01-28 23:24:39 -0500

    CollectionView Showing Bars added to Bottom of MapView
    Continued UI work on the MapView portion of the app. Now displays a horizontal
    collection view showing the bar information so the user can navigate the map
    while seeing a some details about the bars as well. BarCollectionViewCell nib
    along with Cocoa Touch Class for BarCollectionViewCell created for this purpose

---
+++ YourPerfectBar/BarCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BarCollectionViewCell.swift
+//  YourPerfectBar
+//
+//  Created by Kiana Dyson on 1/28/22.
+//
+
+import UIKit
+
+class BarCollectionViewCell: UICollectionViewCell {
+	@IBOutlet var barTitleLabel: UILabel!
+	@IBOutlet var barImageView: UIImageView!
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- YourPerfectBar/MapViewController.swift
+++ YourPerfectBar/MapViewController.swift
@@ -8,15 +8,47 @@
 import UIKit
 import MapKit
 import CoreLocation
+import CDYelpFusionKit
 
 class MapViewController: UIViewController {
+	let yelpAPIClient = CDYelpAPIClient(apiKey: Configuration().yelpApiKey)
+	var bars = [CDYelpBusiness]()
 	
 	private(set) var LOCATION_ZOOM_LEVEL: CLLocationDegrees = 0.05
 	let screenSize: CGRect = UIScreen.main.bounds
 	var locationManager: CLLocationManager?
 	var mapView: MKMapView!
+	var collectionView: UICollectionView!
 	
 	let INITIAL_COORDINATE: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 40.7580, longitude: -73.9855)
+	
+	override func loadView() {
+		view = UIView()
+		view.backgroundColor = .white
+		// MapView setup
+		mapView = MKMapView(frame: view.bounds)
+		mapView.showsUserLocation = true
+		mapView.delegate = self
+		mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		let initialRegion = MKCoordinateRegion(center: INITIAL_COORDINATE, span: MKCoordinateSpan(latitudeDelta: LOCATION_ZOOM_LEVEL, longitudeDelta: LOCATION_ZOOM_LEVEL))
+		mapView.setRegion(initialRegion, animated: true)
+		
+		// CollectionView setup
+		let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+		layout.itemSize = CGSize(width: 300, height: 200)
+		layout.scrollDirection = .horizontal
+		collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+		collectionView.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(mapView)
+		view.addSubview(collectionView)
+		NSLayoutConstraint.activate(
+			[collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10.00),
+			 collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+			 collectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+			 collectionView.heightAnchor.constraint(equalToConstant: 200.00)
+			]
+		)
+	}
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,14 +65,37 @@ class MapViewController: UIViewController {
 		locationManager?.delegate = self
 		locationManager?.requestAlwaysAuthorization()
 		
-		// MapView setup
-		mapView = MKMapView(frame: view.bounds)
-		mapView.showsUserLocation = true
-		mapView.delegate = self
-		mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-		let initialRegion = MKCoordinateRegion(center: INITIAL_COORDINATE, span: MKCoordinateSpan(latitudeDelta: LOCATION_ZOOM_LEVEL, longitudeDelta: LOCATION_ZOOM_LEVEL))
-		mapView.setRegion(initialRegion, animated: true)
-		view.addSubview(mapView)
+		// Collection View setup
+		collectionView.backgroundColor = .clear
+		collectionView.register(UINib(nibName: "BarCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Bar")
+		collectionView.showsHorizontalScrollIndicator = false
+		collectionView.delegate = self
+		collectionView.dataSource = self
+		
+		#warning("api calls should be moved off main thread")
+		#warning("exact api calls being made twice- make optimization to somehow share the data returned by this")
+		yelpAPIClient.searchBusinesses(
+			byTerm: "bars",
+			location: "New York City",
+			latitude: nil,
+			longitude: nil,
+			radius: 5000,
+			categories: nil,
+			locale: .english_unitedStates,
+			limit: 20,
+			offset: nil,
+			sortBy: .bestMatch,
+			priceTiers: nil,
+			openNow: nil,
+			openAt: nil,
+			attributes: nil) { [weak self] cdYelpSearchResponse in
+			guard let businesses = cdYelpSearchResponse?.businesses else { return }
+			self?.bars = businesses
+			self?.collectionView.reloadData()
+			self?.collectionView.scrollToItem(at: IndexPath(row: 4, section: 0), at: .centeredHorizontally, animated: false)
+		}
+		
+		
     }
 	
 	@objc func showListView() {
@@ -66,6 +121,22 @@ extension MapViewController: CLLocationManagerDelegate {
 			//
 		}
 	}
+}
+extension MapViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return bars.count
+	}
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Bar", for: indexPath) as? BarCollectionViewCell else { return UICollectionViewCell() }
+		let bar = bars[indexPath.row]
+		cell.barTitleLabel.text = bar.name
+		cell.barImageView.contentMode = .scaleAspectFill
+		#warning("Look into if the preferred way is to have direct access to the imageView like this")
+		cell.barImageView.sd_setImage(with: bar.imageUrl, placeholderImage: UIImage(systemName: "music.house"))
+		return cell
+	}
+	
+	
 	
 }
 extension MapViewController: MKMapViewDelegate {
