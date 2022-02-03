700328da7c046dc95adae7ac2b4fe5e5ca0722e3
Kiana Dyson <kdyson@calpoly.edu>
2022-02-03 12:33:03 -0500

    Revert "Merge pull request #1 from K-DysonM/Subbranch_DrawRegion_CollectionViewAnimation"
    This reverts commit 3a3897e3b97fe7b2070cc53559a40230a51c8c27, reversing
    changes made to 1b3ce7c14be78bbb48e816e58800adcadc674640.

--- YourPerfectBar/MapViewController.swift
+++ YourPerfectBar/MapViewController.swift
@@ -19,16 +19,25 @@ class MapViewController: UIViewController {
 	let screenSize: CGRect = UIScreen.main.bounds
 	var locationManager: CLLocationManager?
 	var mapView: MKMapView!
+<<<<<<< HEAD
+=======
+	var button: UIButton!
+>>>>>>> parent of 3a3897e... Merge pull request #1 from K-DysonM/Subbranch_DrawRegion_CollectionViewAnimation
 	var collectionView: UICollectionView!
 	
+	
+<<<<<<< HEAD
+=======
 	let INITIAL_COORDINATE: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 40.7580, longitude: -73.9855)
 	
+>>>>>>> parent of 3a3897e... Merge pull request #1 from K-DysonM/Subbranch_DrawRegion_CollectionViewAnimation
 	override func loadView() {
 		view = UIView()
 		view.backgroundColor = .white
 		// MapView setup
 		mapView = MKMapView()
 		mapView.showsUserLocation = true
+		mapView.delegate = self
 		mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 		mapView.translatesAutoresizingMaskIntoConstraints = false
 		
@@ -78,6 +87,7 @@ class MapViewController: UIViewController {
 		// MapView setup
 		let initialRegion = MKCoordinateRegion(center: INITIAL_COORDINATE, span: MKCoordinateSpan(latitudeDelta: LOCATION_ZOOM_LEVEL, longitudeDelta: LOCATION_ZOOM_LEVEL))
 		mapView.setRegion(initialRegion, animated: true)
+<<<<<<< HEAD
 		mapView.delegate = self
 		
 		#warning("api calls should be moved off main thread")
@@ -96,6 +106,34 @@ class MapViewController: UIViewController {
 			mapView.addAnnotation(barMKAnnotation)
 			barAnnotations.append(barMKAnnotation)
 		}
+=======
+		mapView.isUserInteractionEnabled = true
+
+		self.searchForBarsAt(coordinate: nil, location: "New York City")
+    }
+	/// Adds an array of bars as BarMKAnnotations to the map view
+	func addMKAnnotations(forBars list: [CDYelpBusiness]?) {
+		guard let list = list else { return }
+		let newAnnotations = list.compactMap { convertToBarMKAnnotation(from: $0) }
+		mapView.addAnnotations(newAnnotations)
+		barAnnotations += newAnnotations
+	}
+	
+	func convertToBarMKAnnotation(from business: CDYelpBusiness) -> BarMKAnnotation? {
+		let latitude = business.coordinates?.latitude
+		let longitude = business.coordinates?.longitude
+		guard let latitude = latitude, let longitude = longitude  else { return nil }
+		let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+		let barMKAnnotation = BarMKAnnotation(id: business.id, coordinate: location, name: business.name, rating: business.rating)
+		return barMKAnnotation
+	}
+	
+	
+	/// Removes an array of BarMKAnnotation objects from the map view.
+	func removeMKAnnotations(forBars list: [BarMKAnnotation]?) {
+		guard let list = list else { return }
+		mapView.removeAnnotations(list)
+>>>>>>> parent of 3a3897e... Merge pull request #1 from K-DysonM/Subbranch_DrawRegion_CollectionViewAnimation
 	}
 	#warning("This removes all annotations- instead remove annotations that arent in the mapView ")
 	func removeMKAnnotations() {
@@ -110,6 +148,7 @@ class MapViewController: UIViewController {
 	}
 	
 	func searchForBarsAt(coordinate: CLLocationCoordinate2D?, location: String?) {
+<<<<<<< HEAD
 		yelpAPIClient.searchBusinesses(
 			byTerm: "bars",
 			location: location,
@@ -132,6 +171,46 @@ class MapViewController: UIViewController {
 			self?.collectionView.scrollToItem(at: IndexPath(row: middleIndex, section: 0), at: .centeredHorizontally, animated: false)
 			self?.updateMKAnnotations()
 			self?.removeMKAnnotations()
+=======
+		DispatchQueue.global().async {
+			self.yelpAPIClient.searchBusinesses(
+				byTerm: "bars",
+				location: location,
+				latitude: coordinate?.latitude,
+				longitude: coordinate?.longitude,
+				radius: 5000,
+				categories: nil,
+				locale: .english_unitedStates,
+				limit: 50,
+				offset: nil,
+				sortBy: .bestMatch,
+				priceTiers: nil,
+				openNow: nil,
+				openAt: nil,
+				attributes: nil) {[weak self] cdYelpSearchResponse in
+				guard let businesses = cdYelpSearchResponse?.businesses else { return }
+				self?.barsModel.bars = []
+				// ONLY SHOW ANNOTATIONS IF WITHIN THE FRAME -
+				for business in businesses {
+					if let latitude = business.coordinates?.latitude, let longitude = business.coordinates?.longitude {
+						let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+						if let contains = self?.mapView.visibleMapRect.contains(MKMapPoint(coordinate)) {
+							if contains {
+								self?.barsModel.bars.append(business)
+							}
+						}
+					}
+				}
+				let middleIndex = businesses.count/2
+				
+				DispatchQueue.main.async {
+					self?.collectionView.reloadData()
+					self?.collectionView.scrollToItem(at: IndexPath(row: middleIndex, section: 0), at: .centeredHorizontally, animated: false)
+					self?.removeMKAnnotations(forBars: self?.barAnnotations)
+					self?.addMKAnnotations(forBars: self?.barsModel.bars)
+				}
+			}
+>>>>>>> parent of 3a3897e... Merge pull request #1 from K-DysonM/Subbranch_DrawRegion_CollectionViewAnimation
 		}
 	}
 	
@@ -159,13 +238,24 @@ extension MapViewController: CLLocationManagerDelegate {
 		}
 	}
 }
+<<<<<<< HEAD
+=======
+// COLLECTIONVIEW METHODS
+>>>>>>> parent of 3a3897e... Merge pull request #1 from K-DysonM/Subbranch_DrawRegion_CollectionViewAnimation
 extension MapViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 		return barsModel.bars.count
 	}
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+<<<<<<< HEAD
 		guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Bar", for: indexPath) as? BarCollectionViewCell else { return UICollectionViewCell() }
 		let bar = barsModel.bars[indexPath.row]
+=======
+		guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Bar", for: indexPath) as? BarCollectionViewCell else {
+			fatalError("BarCollectionViewCell not properly initialized")
+		}
+		let bar = barsModel.bars[indexPath.item]
+>>>>>>> parent of 3a3897e... Merge pull request #1 from K-DysonM/Subbranch_DrawRegion_CollectionViewAnimation
 		cell.barTitleLabel.text = bar.name
 		cell.barImageView.contentMode = .scaleAspectFill
 		#warning("Look into if the preferred way is to have direct access to the imageView like this")
@@ -173,6 +263,10 @@ extension MapViewController: UICollectionViewDelegate, UICollectionViewDataSourc
 		return cell
 	}
 }
+<<<<<<< HEAD
+=======
+// MAPVIEW METHODS
+>>>>>>> parent of 3a3897e... Merge pull request #1 from K-DysonM/Subbranch_DrawRegion_CollectionViewAnimation
 extension MapViewController: MKMapViewDelegate {
 	func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
 		collectionView.isHidden = true
@@ -200,7 +294,23 @@ extension MapViewController: MKMapViewDelegate {
 		}) else { return }
 		#warning("Bug where if animated is true it doesn't scrollToItem")
 		collectionView.isHidden = false
+<<<<<<< HEAD
 		collectionView.scrollToItem(at: IndexPath(row: index, section: 0), at: .centeredHorizontally, animated: false)
+=======
+		print(index)
+		collectionView.scrollToItem(at: IndexPath(row: index, section: 0), at: .centeredHorizontally, animated: false)
+	}
+	func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+		if overlay is MKPolygon {
+			let polygonView = MKPolygonRenderer(overlay: overlay)
+			polygonView.strokeColor = UIColor(named: "HunterGreen")
+			polygonView.lineWidth = 5
+			polygonView.fillColor = .lightGray.withAlphaComponent(0.3)
+			
+			return polygonView
+		}
+		return MKPolylineRenderer(overlay: overlay)
+>>>>>>> parent of 3a3897e... Merge pull request #1 from K-DysonM/Subbranch_DrawRegion_CollectionViewAnimation
 	}
 	
 }
